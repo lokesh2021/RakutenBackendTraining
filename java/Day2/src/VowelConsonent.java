e909bc4ae041e475a51fd2aa1a5842a599d1c2e5
Lokesh B M <43576996+lokesh2021@users.noreply.github.com>
2022-06-02 13:21:10 +0530

    VowelsAndConsonents

---
+++ java/Day2/src/VowelConsonent.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class VowelConsonent
+{
+    public static void main(String args[])
+    {
+        String input;
+        System.out.println("Enter Input: ");
+        Scanner scan=new Scanner(System.in);
+        input=scan.next();
+
+        char ch=input.charAt(0);
+        if(ch>='a'&&ch<='z' || ch>='A'&&ch<='Z' )
+        {
+            System.out.print("Entered input is an Alphabet and ");
+
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+            {
+                System.out.println("It's a Vowel");
+            }
+            else
+            {
+                System.out.println("It's a Consonent");
+            }
+
+        }
+        else
+        {
+            System.out.println("Entered input is not an Alphabet");
+        }
+
+
+
+    }
+}
